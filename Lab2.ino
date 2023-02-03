ec17c7de372b271e5821c9b616330e96e716e927
Brian Keegan <bkeegan3@jh.edu>
2023-02-02 21:49:59 -0500

    reading from DHT22

--- Lab2.ino
+++ Lab2.ino
@@ -1,9 +1,30 @@
+#include "DHT.h"
+
+#define DHTPIN 2 
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  dht.begin();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(2000); // read every 2 second
+
+  float temp = dht.readTemperature(true); // read as °F with true
+
+  if (isnan(temp)){
+    Serial.println("Missed DHT measurement");
+    return;
+  }
+
+  Serial.print(F("%  Temperature: "));
+  Serial.print(temp);
+  Serial.println(F("°F"));
 
 }
