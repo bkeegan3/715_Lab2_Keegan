ee8c923245e6907ca1b9078e9048d65488ef9db1
Brian Keegan <bkeegan3@jh.edu>
2023-02-03 16:29:14 -0500

    got tim1 to interrupt ever 4.194 seconds

--- Lab2.ino
+++ Lab2.ino
@@ -1,7 +1,11 @@
 #include "DHT.h"
+#include <TimeInterrupt.h>
+#include <avr/io.h>
 
 #define DHTPIN 2 
 #define DHTTYPE DHT22
+#define Temp_delay 10000
+#define ledPin 9
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -9,12 +13,19 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   dht.begin();
+  pinMode(ledPin, OUTPUT);
+  tim1Init();
+
+  sei(); // enable interrupts
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(2000); // read every 2 second
+  // put your main code here, to run repeatedly
+
+}
+
+void read_DHT (){
 
   float temp = dht.readTemperature(true); // read as °F with true
 
@@ -23,8 +34,28 @@ void loop() {
     return;
   }
 
+  // digitalWrite(ledPin, !digitalRead(ledPin)); // toggle the led pin
+
   Serial.print(F("%  Temperature: "));
   Serial.print(temp);
   Serial.println(F("°F"));
+ 
 
 }
+
+void tim1Init(){
+  TCCR1A = 0; // Reset timer1, configure for normal mode
+		
+  // clock_prescale=clk/1024			
+  TCCR1B |= (1<<CS12) |(1<<CS10);
+  TCCR1B &= ~(1<<CS11);
+
+  TIMSK1 = (TOIE1|1); 	// Set timer1 for overflow interrupt enable
+  
+}
+
+ISR(TIMER1_OVF_vect){
+	digitalWrite(ledPin, !digitalRead(ledPin));
+}
+
+
